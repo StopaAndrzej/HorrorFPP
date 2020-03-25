214c8792c7ec2ecf443da43d5300b2c9b10167e5
StopaAndrzej <37457597+StopaAndrzej@users.noreply.github.com>
2020-03-25 19:03:37 +0100

    Leaning left right

--- HorrorFPP/Assets/Scripts/PlayerMove.cs
+++ HorrorFPP/Assets/Scripts/PlayerMove.cs
@@ -21,12 +21,25 @@ public class PlayerMove : MonoBehaviour
 
     [SerializeField] private KeyCode jumpKey;
     [SerializeField] private KeyCode runKey;
+    [SerializeField] private KeyCode LeftLeanKey, RightLeanKey;
 
     private bool isJumping = false;
+
+    [SerializeField] private Transform camera;
+
+    //leaning params
+    [SerializeField] private float leanAngle = 35.0f;
+    [SerializeField] private float leanSpeed = 5.0f;
+    [SerializeField] private float leanBackSpeed = 6.0f;
 
     private void Awake()
     {
         charController = GetComponent<CharacterController>();
+    }
+
+    private void Start()
+    {
+        camera = GetComponent<Transform>();
     }
 
     private void Update()
@@ -49,6 +62,7 @@ public class PlayerMove : MonoBehaviour
 
         SetMovementSpeed();
         JumpInput();
+        CheckLeaning();
     }
 
     private void SetMovementSpeed()
@@ -103,4 +117,61 @@ public class PlayerMove : MonoBehaviour
         charController.slopeLimit = 45.0f;
         isJumping = false;
     }
+
+    private void CheckLeaning()
+    {
+        if(Input.GetKey(LeftLeanKey))
+        {
+            Lean(-1.0f);
+        }
+        else if(Input.GetKey(RightLeanKey))
+        {
+            Lean(1.0f);
+        }
+        else
+        {
+            Lean(0.0f);
+        }
+    }
+
+    private void Lean(float value)
+    {
+        float currAngle = camera.rotation.eulerAngles.z;
+        float targetAngle;
+
+        if (value == -1.0f)
+        {
+            targetAngle = leanAngle;
+            if (currAngle > 180.0)
+            {
+                currAngle = 360.0f - currAngle;
+            }
+        }  
+
+        else if (value == 1.0f)
+        {
+            targetAngle = leanAngle - 360.0f;
+            if (currAngle > 180.0)
+            {
+                targetAngle = 360.0f - leanAngle;
+            }
+        }  
+
+        else
+        {
+            targetAngle = 0.0f;
+            if (currAngle > 180.0)
+            {
+                targetAngle = 360.0f;
+            }
+        }
+            
+
+       
+
+        float angle = Mathf.Lerp(currAngle, targetAngle, leanSpeed * Time.deltaTime);
+        Quaternion rotAngle = Quaternion.Euler(camera.rotation.eulerAngles.x, camera.rotation.eulerAngles.y, angle);
+        camera.rotation = rotAngle;
+    }
+
 }
