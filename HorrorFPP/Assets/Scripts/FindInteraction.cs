53611fb55117072e03f9a31bb364eb2e0acb2f76
StopaAndrzej <37457597+StopaAndrzej@users.noreply.github.com>
2020-03-27 18:22:03 +0100

    +crosshair dot

---
+++ HorrorFPP/Assets/Scripts/FindInteraction.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindInteraction : MonoBehaviour
+{
+    private GameObject raycastedObject;
+
+    [SerializeField] private float rayLength = 10.0f;
+    [SerializeField] private LayerMask layerMaskInteract;
+    [SerializeField] private KeyCode interactButton;
+
+
+    private void Update()
+    {
+        RaycastHit hit;
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+        if(Physics.Raycast(transform.position, fwd, out hit, rayLength, layerMaskInteract.value))
+        {
+            if(hit.collider.CompareTag("Object"))
+            {
+                raycastedObject = hit.collider.gameObject;
+                Debug.Log("Interactive object found!");
+
+                if(Input.GetKeyDown(interactButton))
+                {
+                    Debug.Log("DO IT!");
+                }
+            }
+        }
+    }
+}
