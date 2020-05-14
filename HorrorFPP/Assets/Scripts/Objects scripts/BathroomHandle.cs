b910da82887499674b72eacc582d0ab5ec23b908
StopaAndrzej <37457597+StopaAndrzej@users.noreply.github.com>
2020-05-15 00:53:47 +0200

    3d assets, home scene built

---
+++ HorrorFPP/Assets/Scripts/Objects scripts/BathroomHandle.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BathroomHandle : InteractableObjectBase
+{
+    private bool isOpen = false;
+
+    [SerializeField] private Animator HandleAnimator;
+    [SerializeField] private Animator DoorAnimator;
+    [SerializeField] private Animator LockAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HandleAnimator = GetComponent<Animator>();
+    }
+
+    public override void Interact()
+    {
+        if(!isOpen)
+        {
+            interactText = "Open";
+            HandleAnimator.SetBool("isOpen", true);
+            LockAnimator.SetBool("isOpen", true);
+        }
+        else
+        {
+            DoorOpen();
+        }
+    }
+
+    public void DoorOpen()
+    {
+        if (!isOpen)
+        {
+            DoorAnimator.SetBool("isOpen", true);
+            HandleAnimator.SetBool("isOpen", false);
+            LockAnimator.SetBool("isOpen", false);
+            isOpen = true;
+        }  
+        else
+        {
+            DoorAnimator.SetBool("isOpen", false);
+            isOpen = false;
+        }
+           
+    }
+
+}
